db56cc803a5146239c44a90361993485950c23a8
Adrien Prokopowicz <adrien.prokopowicz@gmail.com>
2017-01-02 16:56:15 +0100

    Improved mobile and tablet display.

--- assets/main.js
+++ assets/main.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
         var deletionSelect = document.getElementById("expires_after");
         var deletionCheckbox = document.getElementById("expires");
         var infoParagraph = makeInfoParagraph();
+        var buttonFieldset = document.createElement("fieldset");
+        var optionsButton = document.createElement("button");
         var validFileType = false;
         var validFileSize = false;
         var dragStatus = 0;
@@ -17,8 +19,21 @@ document.addEventListener("DOMContentLoaded", function() {
         
         document.body.className = "js";
         fileInput.addEventListener("change", updateForm);
+        submitInput.parentNode.replaceChild(buttonFieldset, submitInput);
+        optionsButton.textContent = "🔧";
+        optionsButton.id = "optionsButton";
+        optionsButton.addEventListener("click", function(e) {
+            getDrawer("options").open = true;
+            Dropdown.open();
+            e.preventDefault();
+            return false;
+        });
+        buttonFieldset.appendChild(optionsButton);
+        buttonFieldset.appendChild(submitInput);
+        buttonFieldset.id = "buttons";
+
         updateForm();
-        
+
         function preventDrop(e) {
             if(e.target === fileInput) return;
             e.dataTransfer.dropEffect = 'none';
@@ -42,7 +57,6 @@ document.addEventListener("DOMContentLoaded", function() {
         document.documentElement.addEventListener("dragleave", dragStop);
         fileInput.addEventListener("change", dragStop);
         
-
         deletionSelect.addEventListener("change", function(e) {
             deletionCheckbox.checked = true;
         });
@@ -143,7 +157,7 @@ document.addEventListener("DOMContentLoaded", function() {
         for(var i = 0; i < linkblocks.length; ++i) {
             var linkblock = linkblocks[i];
             var button = linkblocks[i].appendChild(document.createElement("button"));
-            button.textContent = "C";
+            button.textContent = "📋";
             button.title = "Copier dans le presse-papiers";
             
             button.addEventListener("click", function(button) {
@@ -167,13 +181,13 @@ document.addEventListener("DOMContentLoaded", function() {
                     console.error("Error while executing copy command: %o", e);
                     button.setAttribute("data-tooltip", "Erreur lors de la copie");
                 }
-                
+
                 button.className = "tooltip-visible";
                 if(button.timeoutId) clearTimeout(button.timeoutId);
                 button.timeoutId = setTimeout(function() {
                     button.className = "";
                 }, 2000);
-                
+
                 document.body.removeChild(copyBuffer);
             });
 
@@ -183,6 +197,98 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         }
     })();
+
+
+    var drawers = {};
+
+    var Dropdown = {
+        id: "main-dropdown",
+        timeout: 2000,
+        element: document.createElement("div"),
+        _timeoutId: null,
+
+        init: function()
+        {
+            Dropdown.element.id = Dropdown.id;
+            Dropdown.element.addEventListener("click", closeAll);
+        },
+
+        open: function()
+        {
+            document.body.appendChild(Dropdown.element);
+            Dropdown.element.className = "visible";
+            if(Dropdown._timeoutId)
+                clearTimeout(Dropdown._timeoutId);
+        },
+
+        close: function()
+        {
+            Dropdown.element.className = "";
+            Dropdown._timeoutId = setTimeout(Dropdown._onAnimationEnd, Dropdown.timeout);
+        },
+
+        _onAnimationEnd: function()
+        {
+            document.body.removeChild(Dropdown.element);
+            Dropdown._timeoutId = null;
+        }
+    };
+
+    var NavigationDrawer = {
+        id: '',
+
+        get element()
+        {
+            return document.getElementById(this.id)
+                || console.error("Invalid drawer id: %s", this.id);
+        },
+
+        new: function(drawer_id) {
+            var obj = Object.create(this);
+            obj.id = drawer_id;
+            return obj;
+        },
+
+        get open()
+        {
+            return this.element.className.split(" ").indexOf("visible") >= 0;
+        },
+
+        set open(isOpen)
+        {
+            this.element.className = isOpen ? "visible" : "";
+        }
+    };
+
+    function getDrawer(drawer_id)
+    {
+        var drawer = drawers[drawer_id];
+
+        if(!drawer)
+        {
+            drawer = NavigationDrawer.new(drawer_id);
+            drawers[drawer_id] = drawer;
+        }
+
+        return drawer;
+    }
+
+    function openDrawer()
+    {
+        getDrawer(this.getAttribute("data-drawer")).open = true;
+        Dropdown.open();
+    }
+
+    function closeAll()
+    {
+        for(var drawer_id in drawers)
+        {
+            drawers[drawer_id].open = false;
+        }
+        Dropdown.close();
+    }
+
+    Dropdown.init();
 });
 
 
@@ -259,3 +365,5 @@ function get_images()
 
     return images;
 }
+
+
