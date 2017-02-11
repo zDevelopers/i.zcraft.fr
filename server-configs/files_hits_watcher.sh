ca57f4f6f685df1d64484a5fe3149e38616629e4
Amaury Carrade <amaury@carrade.eu>
2017-02-11 15:21:47 +0100

    Re-added --quied in hits watcher removed by error.
    * BUG: --quied missing in the files hits watcher.

--- server-configs/files_hits_watcher.sh
+++ server-configs/files_hits_watcher.sh
@@ -7,6 +7,6 @@ while read line ; do
         echo "$line" | grep -E "GET \/(mini_)?([0-9]+)\\.(jpg|jpeg|gif|bmp|ico|png|BMP|JPG|JPEG|PNG|SVG|svg)"
         if [ $? = 0 ]
         then
-            php /home/www/i.zcraft.fr/console i:hit $(echo "$line" | cut -d '"' -f2 | cut -d ' ' -f2)
+            php /home/www/i.zcraft.fr/console i:hit --quiet $(echo "$line" | cut -d '"' -f2 | cut -d ' ' -f2)
         fi
 done
